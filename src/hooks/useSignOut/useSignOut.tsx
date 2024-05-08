6e0a2419f50c077f2f914fa878afc8ef41e3aef6
Lucas Abreu <elimahgbfq@gmail.com>
2024-05-08 02:32:26 -0300

    Sign out hook correction

--- src/hooks/useSignOut/useSignOut.tsx
+++ src/hooks/useSignOut/useSignOut.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 export const useSignOut = () => {
   const navigate = useNavigate();
   function handleSignOut() {
-    localStorage.removeItem("logged-user");
+    sessionStorage.removeItem("logged-user");
     return navigate("/");
   }
   return { handleSignOut };
